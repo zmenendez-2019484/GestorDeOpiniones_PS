1002b1b478231d6c870387e0f3778ed5d2d123d8
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-02-27 15:25:15 -0600

    Se termina la conexión con el servidor y la base de datos

--- configs/server.js
+++ configs/server.js
@@ -1,39 +1,39 @@
 'use strict'
-import express from 'express'
-import cors from 'cors'
-import helmet from 'helmet'
-import morgan from 'morgan'
-import { dbConnection } from './mongo.js'
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import { dbConnection } from './mongo.js';
 
-class Server {
-    constructor() {
+class Server{
+    constructor(){
+
         this.app = express();
         this.port = process.env.PORT;
-        this.authPath = 'opinionsSystem/v1/auth';
+
         this.middlewares();
-        this.listen();
-        //this.routes();
+        this.conectarDB();
+
     }
-    async connectDB() {
+    async conectarDB() {
         await dbConnection();
     }
 
     middlewares(){
-        this.app.use(express.urlencoded({extended: false}));
+        this.app.use(express.urlencoded({ extended: false }));
         this.app.use(cors());
         this.app.use(express.json());
         this.app.use(helmet());
         this.app.use(morgan('dev'));
     }
-    /*routes() {
-        this.app.use(this.authPath, authRoutes);
-    }*/
+
     listen(){
-        this.app.listen(this.port, () => {
-            console.log('Server running on port ', this.port);
+
+        this.app.listen(this.port, () =>{
+            console.log('Server is running in the port', this.port);
         });
     }
-}
 
+}
 
 export default Server;
