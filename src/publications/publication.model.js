0e559d9efa6ec0ac2ad48a15779834c144e4dbc1
zmenendez-2019484 <zmenendez-2019484@kinal.edu.gt>
2024-03-01 21:47:04 -0600

    Se crea el model de publication

---
+++ src/publications/publication.model.js
@@ -0,0 +1,31 @@
+// models/Post.js
+
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    status: {
+        type: Boolean,
+        default: true
+    }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
